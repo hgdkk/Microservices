6b00a537801fbc38012ad90b7cabf6b7a5eadbce
Halil Gedik <halilgedik06@gmail.com>
2022-02-24 12:42:21 +0300

    Add project files.

---
+++ ProductService/Controllers/ProductController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductService.Database;
+using ProductService.Database.Entities;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ProductService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        DatabaseContext db;
+
+        public ProductController()
+        {
+            db = new DatabaseContext();
+        }
+        // GET: api/<ProductController>
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return db.Products.ToList();
+        }
+
+        // GET api/<ProductController>/5
+        [HttpGet("{id}")]
+        public Product Get(int id)
+        {
+            return db.Products.Find(id);
+        }
+
+        // POST api/<ProductController>
+        [HttpPost]
+        public IActionResult Post([FromBody] Product model)
+        {
+            try
+            {
+                db.Products.Add(model);
+                db.SaveChanges();
+                return StatusCode(StatusCodes.Status201Created, model);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex);
+            }
+        }
+
+    }
+}
---
+++ ProductService/Database/DatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProductService.Database.Entities;
+
+namespace ProductService.Database
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"data source=DESKTOP-F7UR2VC;initial catalog=UserMicroservice;persist security info=True; user id=sa; password=xqwoyn739;");
+        }
+    }
+}
